083d2a22b439615f0bde388aa01e60f235b6af5c
perrashijueputas <42298005+perrashijueputas@users.noreply.github.com>
2018-10-13 10:45:07 -0500

    alejandro

---
+++ New Unity Project/Assets/Primerclase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Primerclase : MonoBehaviour
+{
+   class VeryVeryFirstClass
+    {
+        public int num;
+    }
+
+    private void Start()
+    {
+        VeryVeryFirstClass firstClass = new VeryVeryFirstClass();
+        firstClass.num = 1;
+    }
+}
+
+
+
