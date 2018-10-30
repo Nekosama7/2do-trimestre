1695ec40469ad9cfa3e8198bbd7239c3a53790a9
perrashijueputas <42298005+perrashijueputas@users.noreply.github.com>
2018-10-29 20:42:40 -0500

    scrip

---
+++ New Unity Project/Assets/unray.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unray : MonoBehaviour {
+    int aleatorio = Random.Range(0,50);
+    public int[] lista;
+    
+
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
